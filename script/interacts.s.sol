d65a54143b3de8b597ce523260d99578214fa4b4
Mak <antonenko.makar123@gmail.com>
2025-10-30 16:52:01 -0400

    complete project

--- script/interacts.s.sol
+++ script/interacts.s.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
-import { MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 
 contract ClaimAirdrop is Script {
-
     address CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     uint256 CLAIMING_AMOUNT = 25 * 1e18;
     bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
     bytes32 PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
-    bytes private SIGNATURE = hex"eba11dc5bd5bd0f468007303124c13965cadb551ff6b0578aa95bead09724a7333025cf6b4e5f22d74dfb1cc1dbb5ca3a8d2636d2727f7e510b046c76b20e9eb1c";
+    bytes private SIGNATURE =
+        hex"fbd2270e6f23fb5fe9248480c0f4be8a4e9bd77c3ad0b1333cc60b5debc511602a2a06c24085d8d7c038bad84edc53664c8ce0346caeaa3570afec0e61144dc11c";
 
     bytes32[] proof = [PROOF_ONE, PROOF_TWO];
 
@@ -19,13 +19,13 @@ contract ClaimAirdrop is Script {
 
     function claimAirdrop(address airdrop) public {
         vm.startBroadcast();
-        (uint8 v, bytes32 r, bytes32 s) = slpitSignature(SIGNATURE);
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
         MerkleAirdrop(airdrop).claim(CLAIMING_ADDRESS, CLAIMING_AMOUNT, proof, v, r, s);
         vm.stopBroadcast();
     }
 
     function splitSignature(bytes memory sig) public pure returns (uint8 v, bytes32 r, bytes32 s) {
-        if(sig.length != 65) {
+        if (sig.length != 65) {
             revert __ClaimAirdropScript__InvalidSignatureLength();
         }
 
@@ -34,14 +34,12 @@ contract ClaimAirdrop is Script {
             s := mload(add(sig, 64))
             v := byte(0, mload(add(sig, 96)))
         }
-
     }
-
 
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MerkleAirdrop", block.chainid);
         claimAirdrop(mostRecentlyDeployed);
-        
-        //MerkleAirdrop 
+
+        //MerkleAirdrop
     }
 }
